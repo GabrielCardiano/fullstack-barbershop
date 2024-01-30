a1d67f72a3ed78da14c355222695c71da70be5f2
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-30 16:12:33 -0300

    feat: add welcome message in HomePage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,18 @@
+import { format } from "date-fns";
 import Header from "../components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2>Olá, <span className="text-xl font-bold">User</span>!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM 'de' yyyy'.'", { locale: ptBR, })}
+        </p>
+      </div>
     </div>
   );
 }
