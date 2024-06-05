41197ca0b854a83d8de9939030dd0b186bdc7885
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-06-05 15:21:21 -0300

    feat: enable filter barbershops system

---
+++ app/(home)/barbershop/page.tsx
@@ -0,0 +1,41 @@
+import Header from "@/app/_components/header";
+import { db } from "@/app/_lib/prisma";
+import BarbershopItem from "../_components/barbershopItem";
+
+interface BarberShopageProps {
+  searchParams: {
+    search?: string,
+  }
+}
+
+async function BarberShopPage({ searchParams }: BarberShopageProps) {
+  const barbershop = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams.search,
+        mode: 'insensitive'
+      }
+    }
+  });
+
+  return (
+    <>
+      <Header />
+
+      <div className="px-5 py-6">
+        <h1 className="text-gray-400 font-bold text-xs uppercase">
+          Resultados para &quot;{searchParams.search}&quot;
+        </h1>
+        <div className="grid grid-cols-2 mt-3 gap-4">
+          {barbershop.map(barbershop => (
+            <div key={barbershop.id} className="w-full">
+              <BarbershopItem barbershop={barbershop} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BarberShopPage;
