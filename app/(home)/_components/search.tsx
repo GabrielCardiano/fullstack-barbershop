2f73b3e9734c986db06d9115dd7e8362f3d29b21
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-06-06 19:39:57 -0300

    feat: complete barbershop filter system

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -1,16 +1,65 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/app/_components/ui/form";
 import { Input } from "@/app/_components/ui/input";
 import { SearchIcon } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
 
-const Search = () => {
+import { z } from "zod";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const formSchema = z.object({
+  search: z.string({ required_error: "Campo obrigatório" })
+    .trim()
+    .min(1, "Campo obrigatório")
+    .max(50)
+});
+
+
+function Search() {
+  const router = useRouter();
+  const searchParams = useSearchParams().get('search');
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: searchParams ? searchParams : '',
+    }
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershop?search=${data.search}`);
+  }
+
   return (
     <div className="flex items-center gap-2">
-      <Input placeholder="Busque uma barbearia..." />
-      <Button variant="default" >
-        <SearchIcon size={20} />
-      </Button>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full gap-4">
+
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  {/* <Input placeholder="shadcn" {...field} /> */}
+                  <Input placeholder="Busque uma barbearia..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button variant="default" type="submit" >
+            <SearchIcon size={20} />
+          </Button>
+
+        </form>
+      </Form>
+
     </div>
   )
 }
--- app/(home)/barbershop/page.tsx
+++ app/(home)/barbershop/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@/app/_components/header";
 import { db } from "@/app/_lib/prisma";
 import BarbershopItem from "../_components/barbershopItem";
+import { redirect } from "next/navigation";
+import Search from "../_components/search";
 
 interface BarberShopageProps {
   searchParams: {
@@ -9,6 +11,10 @@ interface BarberShopageProps {
 }
 
 async function BarberShopPage({ searchParams }: BarberShopageProps) {
+  if (!searchParams.search) {
+    redirect("/");
+  };
+
   const barbershop = await db.barbershop.findMany({
     where: {
       name: {
@@ -22,10 +28,14 @@ async function BarberShopPage({ searchParams }: BarberShopageProps) {
     <>
       <Header />
 
-      <div className="px-5 py-6">
+
+      <div className="px-5 py-6 flex flex-col gap-6">
+        <Search />
+
         <h1 className="text-gray-400 font-bold text-xs uppercase">
           Resultados para &quot;{searchParams.search}&quot;
         </h1>
+
         <div className="grid grid-cols-2 mt-3 gap-4">
           {barbershop.map(barbershop => (
             <div key={barbershop.id} className="w-full">
